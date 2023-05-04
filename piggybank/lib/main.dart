2720bb3dbcc767cc652e0dc58b36a171599aaf3e
Ana <anatran334@gmail.com>
2023-05-04 16:02:23 -0400

    Update main.dart
    Removed unused code
    Updated theme colors
    Added buttons and app title
    Centered buttons

--- piggybank/lib/main.dart
+++ piggybank/lib/main.dart
@@ -17,7 +17,8 @@ class MyApp extends StatelessWidget {
         title: 'Piggy Bank',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(0, 0, 0, 0)),
+          scaffoldBackgroundColor: Color.fromRGBO(241, 166, 227, 1)
         ),
         home: MyHomePage(),
       ),
@@ -26,26 +27,64 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+  //var current = WordPair.random();
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-
     return Scaffold(
-      body: Column(
-        children: [
-          Center(child: Text(appState.current.asLowerCase)),
-          ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-            },
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AppName(),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // ↓ And this.
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Add Income'),
+                ),
+
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Add Expense'),
+                ),
+
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Set Budget'),
+                ),
+
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('View Spending'),
+                ),
+
+              ],
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class AppName extends StatelessWidget {
+  const AppName({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Piggy Bank',
+    style: TextStyle(
+      fontSize: 30,
+      color: Colors.black87 
+      ),
+      );
   }
 }
