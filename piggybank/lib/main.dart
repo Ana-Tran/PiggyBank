75ad2627142c04f15c52679712e1a49de19da07f
Ana <anatran334@gmail.com>
2023-05-04 18:23:17 -0400

    Update main.dart
    Change button style
    Change button layout

--- piggybank/lib/main.dart
+++ piggybank/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -16,10 +15,20 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Piggy Bank',
         theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(0, 0, 0, 0)),
-          scaffoldBackgroundColor: Color.fromRGBO(241, 166, 227, 1)
-        ),
+            useMaterial3: true,
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 0, 0, 0),
+                foregroundColor: Color.fromARGB(255, 255, 255, 255),
+                padding: const EdgeInsets.all(40),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+                elevation: 3,
+            )),
+            colorScheme:
+                ColorScheme.fromSeed(seedColor: Color.fromARGB(0, 0, 0, 0)),
+            scaffoldBackgroundColor: Color.fromRGBO(241, 166, 227, 1)),
         home: MyHomePage(),
       ),
     );
@@ -44,31 +53,88 @@ class MyHomePage extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               children: [
                 // ↓ And this.
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Add Income'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    AddIncome(),
+                    SizedBox(height: 10, width: 10),
+                    AddExpense(),
+                  ],
                 ),
 
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Add Expense'),
+                SizedBox(height: 15),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    AddBudget(),
+                    SizedBox(height: 10, width: 10),
+                    ViewSpending(),
+                  ],
                 ),
 
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Set Budget'),
-                ),
 
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('View Spending'),
-                ),
-
+                
               ],
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class ViewSpending extends StatelessWidget {
+  const ViewSpending({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Text('View Spending'),
+    );
+  }
+}
+
+class AddBudget extends StatelessWidget {
+  const AddBudget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Text('Set Budget'),
+    );
+  }
+}
+
+class AddExpense extends StatelessWidget {
+  const AddExpense({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Text('Add Expense'),
+    );
+  }
+}
+
+class AddIncome extends StatelessWidget {
+  const AddIncome({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Text('Add Income'),
     );
   }
 }
@@ -80,11 +146,9 @@ class AppName extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('Piggy Bank',
-    style: TextStyle(
-      fontSize: 30,
-      color: Colors.black87 
-      ),
-      );
+    return Text(
+      'Piggy Bank',
+      style: TextStyle(fontSize: 30, color: Colors.black87),
+    );
   }
 }
